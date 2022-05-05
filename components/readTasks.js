186d0a1094fbe3fe33d41aab284274dcb7974a55
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-05-05 16:39:48 -0500

    Accediendo al arreglo de fechas únicas

--- components/readTasks.js
+++ components/readTasks.js
@@ -1,9 +1,16 @@
+import { uniqueDates } from "../services/date.js";
 import { createTask } from "./addTask.js";
+import dateElement from "./dateElement.js";
 
 export const readTasks = () => {
     const list = document.querySelector("[data-list]");
     const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+    const dates = uniqueDates(taskList);
+    dates.forEach((date) => {
+        console.log(date);
+    });
     taskList.forEach((task) => {
+        list.appendChild(dateElement(task.dateFormat));
         list.appendChild(createTask(task));
     });
 }
