0d96ede2ce2a23011e2dc613738ef302516d757b
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-05-05 14:58:06 -0500

    Extrayendo código

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-/**Importando módulos */
-import checkComplete from "./components/checkComplete.js";
-import deleteIcon from "./components/deleteIcon.js";
-
 //Inmediately invoked function expression IIFE
 /** Funciones que en cuanto se declaran se ejecutan
  * La ventaja de ellas es que no quedan al alcance
@@ -19,42 +15,9 @@ import deleteIcon from "./components/deleteIcon.js";
  * Mientras que si solo declaramos las funciones fuera,
  * permanecerán al alcance del usuario.
 */
+import addTask from "./components/addTask.js";
 
 const btn = document.querySelector("[data-form-btn]");
-console.log(btn);
-
-const createTask = (evento) => {
-    /**
-     * En el navegador para mantener los console.log antes de
-     * evitar que se recargue la página seleccionamos en el
-     * icono de engrane la opción Persist Logs
-     */
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    task.classList.add("card");
-    input.value = "";
-
-    //Template de Strings `` backticks
-    /**
-     * Usar Template de String tiene las siguientes ventajas:
-     * 1. Combinar etiquetas HTML con variables de JavaScript.
-     * 2. Para incluír variables es con ${nombreVariable}
-     */
-    const taskContent = document.createElement("div");
-    const titleTask = document.createElement("span");
-    titleTask.classList.add("task");
-    titleTask.innerText = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-    //task.innerHTML = content;
-    task.appendChild(taskContent);
-    task.appendChild(deleteIcon());
-    list.appendChild(task);
-
-}
 
 /**Esta es una Arrow Function o Función anónima */
-btn.addEventListener("click", createTask);
+btn.addEventListener("click", addTask);
