5531d5ad197ce0111c127f6142b6063a86f03bca
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-05-05 21:33:38 -0500

    Localizando elementos por id, y alterando si son true o false

--- components/checkComplete.js
+++ components/checkComplete.js
@@ -1,11 +1,11 @@
-const checkComplete = () => {
+const checkComplete = (id) => {
     const i = document.createElement("i");
     i.classList.add('far', 'fa-check-square', 'icon');
-    i.addEventListener("click", completeTask);
+    i.addEventListener("click", (event) => completeTask(event, id));
     return i;
 }
 
-const completeTask = (event) => {
+const completeTask = (event, id) => {
     const element = event.target;
     /**Función toggle:
      * Verifica si existe o no la clase.
@@ -14,6 +14,14 @@ const completeTask = (event) => {
     element.classList.toggle("fas");
     element.classList.toggle("completeIcon");
     element.classList.toggle("far");
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    //findIndex devuelve la posición en un arreglo
+    const index = tasks.findIndex(item => item.id == id);
+    //Accedemos al index obtenido seguido de la llave con el nombre
+    //A continuacón negamos la misma sentencia para que en caso
+    //Independiente de ser true o false niega el valor.
+    tasks[index]["complete"] = !tasks[index]["complete"]
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 /**Exportando función principal */
